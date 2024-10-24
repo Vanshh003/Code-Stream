57c7a086c4d5bcf1d3627ce1433e1e8927c38eb7
Vansh Aggarwal <106269430+Vanshh003@users.noreply.github.com>
2024-10-24 17:47:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -62,5 +62,5 @@ io.on('connection', (socket) => {
 });
 
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => console.log(`listening on port ${PORT}`));
