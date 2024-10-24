e7b92179100b307abd2b8a1c27725637dc2916f0
Vansh Aggarwal <106269430+Vanshh003@users.noreply.github.com>
2024-10-24 20:28:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const io = new Server(server);
 const userSocketMap = {};
 
 // Health check route
-app.get('/health', (req, res) => {
-    res.status(200).send('OK');
-});
+// app.get('/health', (req, res) => {
+//     res.status(200).send('OK');
+// });
 
 function getAllConnectedClients(roomId) {
     return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
