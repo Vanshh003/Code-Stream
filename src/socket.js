23187f43705a537438dbcb0bce8504dead2ee436
Vansh Aggarwal <106269430+Vanshh003@users.noreply.github.com>
2024-10-24 17:41:37 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -9,7 +9,7 @@ export const initSocket = async () => {
     };
 
     console.log(process.env);
-    const socket = io("ws://code-stream-1rn1.onrender.com:" + process.env.PORT, options);
+    const socket = io("https://code-stream-1rn1.onrender.com:8000", options);
     
     socket.on('connect_error', (err) => {
         console.error('Socket connection error:', err);
